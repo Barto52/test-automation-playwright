51f618129b315de5c42c2ec305a0b0d0f54bf5fc
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-11-24 18:18:07 +0100

    chore: configure eslint with prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**']},
+    {files: ['**/*.{ ts}']},
+    {languageOptions: {globals: globals.node}},
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            'no-console': warn,
+        },
+    },
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            '@typescript-eslint/explicit-function-return-type': 'error',
+        },
+    },
+    eslintPluginPrettierRecommended, 
+];
