a75ab857eaa60eeb4bb51951c3438f3431e11ad2
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-05 19:47:30 +0100

    refactor: change envConfig urls

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -2,14 +2,7 @@ import {EnvConfig} from '@_src/interfaces/envConfig';
 
 export const envConfig: EnvConfig = {
     URL: {
-        homepageURL: 'https://automationexercise.com/',
-        loginAndRegisterPageURL: 'https://automationexercise.com/login',
-        productsPageURL: 'https://automationexercise.com/products',
-        cartPageURL: 'https://automationexercise.com/view_cart',
-        contactUsPageURL: 'https://automationexercise.com/contact_us',
+        homepageURL: 'http://localhost:3000',
     },
-    API: {
-        verifyLoginEndpoint: 'https://automationexercise.com/api/verifyLogin',
-        deleteUserEndpoint: 'https://automationexercise.com/api/deleteAccount',
-    },
+    API: {},
 };
--- src/interfaces/envConfig.ts
+++ src/interfaces/envConfig.ts
@@ -1,15 +1,8 @@
 export interface URLConfig {
     homepageURL: string;
-    loginAndRegisterPageURL: string;
-    productsPageURL: string;
-    cartPageURL: string;
-    contactUsPageURL: string;
 }
 
-export interface APIConfig {
-    verifyLoginEndpoint: string;
-    deleteUserEndpoint: string;
-}
+export interface APIConfig {}
 
 export interface EnvConfig {
     URL: URLConfig;
