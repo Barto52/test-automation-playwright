51dccc09b97f45c0e81d2e88523b54dbf396f9fd
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-08 16:53:18 +0100

    chore: add RegisterPageEnum with inputs placeholders and page labels

---
+++ src/enums/registerPage.enum.ts
@@ -0,0 +1,9 @@
+export enum RegisterPageEnum {
+    BirthDateLabel = 'Enter Birth Date',
+    EmailLabel = 'Enter User Email',
+    FirstNameLabel = 'Enter User First Name',
+    LastNameLabel = 'Enter User Last Name',
+    PageLabel = 'Register',
+    PasswordLabel = 'Enter Password',
+    RegisterButtonLabel = 'Register',
+}
