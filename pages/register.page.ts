a8b36a0ea56ab465bcb30b61b855f41bfb1414c8
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-10 22:51:42 +0100

    chore: add register.page.ts file with locators

---
+++ pages/register.page.ts
@@ -0,0 +1,24 @@
+import {Page} from '@playwright/test';
+
+export class RegisterPage {
+    constructor(private page: Page) {}
+
+    //Buttons//
+    formRegisterButton = this.page.locator('[data-testid="register-button"]');
+
+    //Dropdowns//
+    avatarSelectionDropdown = this.page.locator('select#avatar ');
+
+    //Inputs//
+    userBirthdateInput = this.page.locator('[data-testid="birthdate-input"]');
+    userEmailInput = this.page.locator('[data-testid="email-input"]');
+    userFirstNameInput = this.page.locator('[data-testid="firstname-input"]');
+    userLastNameInput = this.page.locator('[data-testid="lastname-input"]');
+    userPasswordInput = this.page.locator('[data-testid="password-input"]');
+
+    //Images//
+    userAvatar = this.page.locator('#userPicture');
+
+    //Labels//
+    pageLabel = this.page.locator("form[id='registerForm'] div h2");
+}
