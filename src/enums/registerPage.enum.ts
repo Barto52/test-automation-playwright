2b84031e3b7b5998ea1b8ea0e33f85b0473c95de
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-16 20:10:57 +0100

    chore: add AlertPopupLabel

--- src/enums/registerPage.enum.ts
+++ src/enums/registerPage.enum.ts
@@ -1,9 +1,10 @@
 export enum RegisterPageEnum {
+    AlertPopupLabel = 'User created',
     BirthDateLabel = 'Enter Birth Date',
     EmailLabel = 'Enter User Email',
     FirstNameLabel = 'Enter User First Name',
+    FormRegisterButtonLabel = 'Register',
     LastNameLabel = 'Enter User Last Name',
     PageLabel = 'Register',
     PasswordLabel = 'Enter Password',
-    formRegisterButtonLabel = 'Register',
 }
