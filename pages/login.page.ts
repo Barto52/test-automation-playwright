93a2eafa733af5e5ab97f35d3c915b4fd57e1fac
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-19 19:07:06 +0100

    refactor: fix locators in login.page.ts file

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -4,14 +4,15 @@ export class LoginPage {
     constructor(private page: Page) {}
 
     //Buttons//
-    loginButton = this.page.locator('#userDropdownLoginButton');
+    loginButton = this.page.locator('#loginButton');
     //Checkboxes//
     keepMeSignInCheckbox = this.page.locator('#keepSignIn');
 
     //Inputs//
-    userEmailInput = this.page.locator('#username');
+    userEmailInput = this.page.locator('.input-field #username');
     userPasswordInput = this.page.locator('#password');
 
     //Labels//
     pageLabel = this.page.locator('[action="/process_login"] h2');
+    checkboxLabel = this.page.locator('label[for="keepSignIn"]');
 }
