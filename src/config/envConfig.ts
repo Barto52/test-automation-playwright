b42660873e22c9b9dc4fe148ef93db9340eb46f7
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-19 19:07:19 +0100

    chore: add accountURL in envConfig.ts file

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -2,6 +2,7 @@ import {EnvConfig} from '@_src/interfaces/envConfig.interface';
 
 const envConfig: EnvConfig = {
     URL: {
+        accountURL: 'http://localhost:3000/welcome',
         homepageURL: 'http://localhost:3000',
         loginURL: 'http://localhost:3000/login/',
         registerURL: 'http://localhost:3000/register.html',
