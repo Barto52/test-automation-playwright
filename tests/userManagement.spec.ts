7c8467607b6f0537da1b68050b727dad459ac139
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2024-12-26 17:57:36 +0100

    test: refactor register user and login tests

--- tests/userManagement.spec.ts
+++ tests/userManagement.spec.ts
@@ -1,20 +1,17 @@
-import {expect, test} from '@playwright/test';
+import {test} from '@playwright/test';
 
 import {AccountPageFlow} from '@_flow/accountPage.flow';
 import {CommonFlow} from '@_flow/common.flow';
 import {LoginPageFlow} from '@_flow/loginPage.flow';
 import {RegisterPageFlow} from '@_flow/registerPage.flow';
-import {LoginPage} from '@_pages/login.page';
-import {RegisterPage} from '@_pages/register.page';
-import envConfig from '@_src/config/envConfig';
 import {UserDataFactory} from '@_src/factory/generateUserData.factory';
 
 test.describe('User Management', () => {
     const userDataFactory = new UserDataFactory();
     const userData = userDataFactory.generateUserData();
+    let userID;
 
     test('Registers a new user via UI', {tag: ['@e2e', '@userManagement']}, async ({page}) => {
-        const registerPage = new RegisterPage(page);
         const commonFlow = new CommonFlow(page);
         const registerPageFlow = new RegisterPageFlow(page);
 
@@ -23,24 +20,22 @@ test.describe('User Management', () => {
         await registerPageFlow.verifyPageLabelsAndElements();
         await registerPageFlow.fillRegistrationForm(userData);
         await registerPageFlow.setRandomAvatarId();
-        await registerPage.registerButton.click();
-        await registerPageFlow.verifyRegisterPopupVisibilityAndText();
-        await page.waitForURL(envConfig.URL.loginURL);
-        await expect(page).toHaveURL(envConfig.URL.loginURL);
+        await registerPageFlow.submitRegisterForm();
     });
 
     test('Log in with a new user account', {tag: ['@e2e', '@userManagement']}, async ({page}) => {
         const loginPageFlow = new LoginPageFlow(page);
         const commonFlow = new CommonFlow(page);
-        const loginPage = new LoginPage(page);
         const accountPageFlow = new AccountPageFlow(page);
 
         await commonFlow.gotoHomepage();
         await commonFlow.gotoLoginPageFromHeaderDropdown();
         await loginPageFlow.verifyPageLabelsAndElements();
         await loginPageFlow.fillLoginForm(userData);
-        await loginPage.loginButton.click();
-        await expect(page).toHaveURL(envConfig.URL.accountURL);
+        await loginPageFlow.submitLoginForm();
         await accountPageFlow.verifyPageLabelVisibilityAndContent(userData);
+
+        userID = await accountPageFlow.gotoMyProfileAndGetUserID();
+        await accountPageFlow.verifyUserIDInURL(userID);
     });
 });
