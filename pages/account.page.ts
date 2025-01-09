9564dd80ba0e7a44ca69f6ff208384c6c2455ce4
Bartosz Kijanka <bartosz.kijanka.qa@gmail.com>
2025-01-09 21:49:43 +0100

    refactor: delete commented lines

--- pages/account.page.ts
+++ pages/account.page.ts
@@ -3,13 +3,8 @@ import {Page} from '@playwright/test';
 export class AccountPage {
     constructor(private page: Page) {}
 
-    //Buttons//
     myProfileButton = this.page.locator('#btnMyAccountLink button');
-    //Checkboxes//
-
-    //Inputs//
-
-    //Labels//
+  
     welcomeMessageLabel = this.page.locator('[data-testid="hello"]');
     userIDLabel = this.page.locator('[data-testid="id"]');
 }
--- pages/common.page.ts
+++ pages/common.page.ts
@@ -3,14 +3,10 @@ import {Page} from '@playwright/test';
 export class CommonPage {
     constructor(private page: Page) {}
 
-    //Buttons//
     userDropdownLoginButton = this.page.locator('#loginBtn');
     userDropdownRegisterButton = this.page.locator('#registerBtn');
 
-    //Dropdowns//
     userDropdown = this.page.locator('[data-testid="user-dropdown"]');
     userDropdownContent = this.page.locator('#dropdown-content');
-    //Inputs//
-
-    //Labels//
+  
 }
